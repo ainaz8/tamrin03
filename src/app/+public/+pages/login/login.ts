57e8b58af5ca2718b374e34c1cc9a27fbfbca859
ainaz8 <rasoliiferi@gmail.com>
2025-12-13 16:05:16 +0330

    finish login

--- src/app/+public/+pages/login/login.ts
+++ src/app/+public/+pages/login/login.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
 export class Login {
-loginform:loginform={
-username:'',
-password:'',
-keepme:false
-};
-message:string='';
+
+  router = inject(Router);
+
+  loginform: LoginForm = {
+    username: '',
+    password: '',
+    keepme: false
+  };
+
+  message: string = '';
+
+  check() {
+    if ( this.loginform.username === 'ainaz' && this.loginform.password === '1384'){
+      sessionStorage.setItem('token','jhgbyhugvgvhgvhg');
+      if(this.loginform.keepme==true){
+        localStorage.setItem('token','jhgbyhugvgvhgvhg');
+      }
+        this.router.navigateByUrl('/private');
+    } 
+    else {
+      this.message = 'نام کاربری یا کلمه عبور صحیح نیست';
+    }
+  }
 }
-export interface loginform{
-  username:string;
-  password:string;
-  keepme:boolean;
+
+export interface LoginForm {
+  username: string;
+  password: string;
+  keepme: boolean;
 }
